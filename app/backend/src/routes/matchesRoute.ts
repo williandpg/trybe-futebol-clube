318a34a61c4e2c89dff6f216b77873271de01c52
Willian Gonçalves <willian.p.g@outlook.com>
2024-01-17 00:11:07 -0300

    Corrigindo erros

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -5,6 +5,14 @@ const matchesController = new MatchesController();
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => matchesController.findAll(req, res));
+router.get('/', (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+
+  if (inProgress === undefined) {
+    matchesController.findAll(req, res);
+  } else {
+    matchesController.findByProgress(req, res);
+  }
+});
 
 export default router;
